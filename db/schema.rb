25213382cf506b47437f0d9f691623c1347c6f96
hiramatch <hiramatch@gmail.com>
2019-07-06 19:07:45 +0900

    Generate scaffold Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_095405) do
+ActiveRecord::Schema.define(version: 2019_07_06_100125) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
